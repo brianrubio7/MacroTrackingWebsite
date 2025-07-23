0a5b8c59951026b65ee3564364a460c418db28be
Brian Rubio <brianjrubio@gmail.com>
2025-07-23 18:26:36 -0400

    Start of welcome page

---
+++ Calorie-Tracker/Munchy/src/Components/WelcomePage.jsx
@@ -0,0 +1,16 @@
+import "../Styling/WelcomePage.css";
+
+const WelcomePage = () => {
+  return (
+    <div class="wrapper">
+      <h1>Welcome to Munchy!</h1>
+      <p>
+        Your personal assistant for tracking calories, macros, and healthy
+        habits.
+      </p>
+      <button>Get Started</button>
+    </div>
+  );
+};
+
+export default WelcomePage;
