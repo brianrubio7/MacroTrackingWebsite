2fe12e0db4986700912c2099d3d01c21008663dd
Brian Rubio <brianjrubio@gmail.com>
2025-09-05 12:14:54 -0400

    Updated

--- Calorie-Tracker/Munchy/src/main.jsx
+++ Calorie-Tracker/Munchy/src/main.jsx
@@ -2,11 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./Styling/index.css";
 import App from "./Components/App";
-import WelcomePage from "./Components/WelcomePage";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-    <WelcomePage />
   </StrictMode>
 );
