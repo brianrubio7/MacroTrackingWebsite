6562ba633b472aaff5e293059ae30f60507a4159
Brian Rubio <brianjrubio@gmail.com>
2025-09-10 17:09:15 -0400

    Initialization of new form

---
+++ Calorie-Tracker/Munchy/src/Components/OnBoarding/WeightForm.jsx
@@ -0,0 +1,14 @@
+import "../../Styling/WeightFrom.css";
+
+const WeightForm = () => {
+  return (
+    <>
+      <div className="wrapper4">
+        <h4>How tall are you?</h4>
+        <input></input>
+      </div>
+    </>
+  );
+};
+
+export default WeightForm;
