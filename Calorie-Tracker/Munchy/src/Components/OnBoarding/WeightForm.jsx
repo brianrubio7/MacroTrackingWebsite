f1bd7053abfffc116a284b9552b673c1229847bf
Brian Rubio <brianjrubio@gmail.com>
2025-09-26 12:41:28 -0400

    weight form completed

--- Calorie-Tracker/Munchy/src/Components/OnBoarding/WeightForm.jsx
+++ Calorie-Tracker/Munchy/src/Components/OnBoarding/WeightForm.jsx
@@ -4,8 +4,27 @@ const WeightForm = ({ onNext, onBack }) => {
   return (
     <>
       <div className="wrapper4">
-        <h4>How tall are you?</h4>
-        <input type="number" placeholder="Weight (lbs)"></input>
+        <h2>How tall are you?</h2>
+        <div className="heightInput">
+          <input type="number" placeholder="Height(feet)"></input>
+          <input type="number" placeholder="Inches(in)"></input>
+        </div>
+        <h2>What is your weight?</h2>
+        <div className="weightInput">
+          <input type="number" placeHolder="Weight (lbs)"></input>
+        </div>
+        <h2>What is your goal weight?</h2>
+        <div className="goalWeightInput">
+          <input type="number" placeHolder="Weight (lbs)"></input>
+        </div>
+        <div className="buttonWrapper2">
+          <button onClick={onBack} className="weightBack">
+            Back
+          </button>
+          <button onClick={onNext} className="weightNext">
+            Next
+          </button>
+        </div>
       </div>
     </>
   );
